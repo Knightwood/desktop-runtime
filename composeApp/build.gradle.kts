b1757c03421b9e6ec79718b8a38e0866c9bdd78e
Knightwood <33772264+Knightwood@users.noreply.github.com>
2025-04-10 15:42:07 +0800

    修改打包java模块

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -101,6 +101,7 @@ compose.desktop {
         }
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            modules("java.instrument", "jdk.unsupported", "java.naming")
             packageName = "com.github.knightwood.example"
             packageVersion = "1.0.0"
         }
